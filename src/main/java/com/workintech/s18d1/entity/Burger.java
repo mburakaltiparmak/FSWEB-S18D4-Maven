366fa955006bee4e34e18835339f6132ef366c57
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-05-10 00:23:37 +0300

    Etütteki çözümlere göre uyarlanarak proje yapıldı. Testte setIsVegan ve getIsVegan kısmında hata alındı (@Data annotation'ı mevcut olmasına rağmen.). Bu hatadan dolayı testten geçmesi için class içerisine ilgili getter ve setter metodları manuel olarak yazıldı. Sonraki committe exceptionlar çözülecek.

---
+++ src/main/java/com/workintech/s18d1/entity/Burger.java
@@ -0,0 +1,35 @@
+package com.workintech.s18d1.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "burger",schema = "fsweb")
+public class Burger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private double price;
+    @Column(name = "is_vegan")
+    private boolean isVegan;
+
+    @Column(name = "bread_type")
+    @Enumerated(EnumType.STRING)
+    private BreadType breadType;
+    @Column(name = "contents")
+    private String contents;
+
+
+    public void setIsVegan(boolean b) {
+        this.isVegan=isVegan;
+    }
+    public boolean getIsVegan(){
+        return isVegan;
+    }
+}
