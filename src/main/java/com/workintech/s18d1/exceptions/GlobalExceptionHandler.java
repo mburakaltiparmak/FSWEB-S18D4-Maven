1a8e8fc156965db9248c197b7baa360d7823a51c
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-05-10 00:39:00 +0300

    Tüm testlerden geçti.

--- src/main/java/com/workintech/s18d1/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/workintech/s18d1/exceptions/GlobalExceptionHandler.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @AllArgsConstructor
+@ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<BurgerErrorResponse> handleException(BurgerException exception){
